9b3c67c7980c0fd6803d36569021c8d4957d4ae0
mateuswetah <mateus.m.luna@gmail.com>
2020-12-16 23:45:51 -0300

    Experiments with triangle collapse shape

--- build/litegraph.js
+++ build/litegraph.js
@@ -7930,9 +7930,10 @@ LGraphNode.prototype.executeAction = function(action)
                         //     Math.PI * 2
                         // );
                         // Draws a triangle instead of a circle
-                        var side = box_size * 0.5;
+                        var side = box_size * 0.75;
                         var h = side * (Math.sqrt(3)/2);
                         ctx.translate(title_height * 0.5, title_height * -0.5);
+                        ctz.rotate(45);
                         ctx.moveTo(0, -h / 2);
                         ctx.lineTo( -side / 2, h / 2);
                         ctx.lineTo(side / 2, h / 2);
@@ -7940,6 +7941,7 @@ LGraphNode.prototype.executeAction = function(action)
                         
                         ctx.stroke();
                         ctx.fill(); 
+                        ctz.rotate(45);
                         ctx.translate(title_height * -0.5, title_height * 0.5);
                     ctx.closePath();
 				}
